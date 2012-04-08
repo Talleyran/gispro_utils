08db5b7b4a5bdb790e688144533468dd372b6369
Kirill Jakovlev <special-k@li.ru>
2012-04-09 00:49:44 +0400

    utils

---
+++ public/javascripts/utils.js
@@ -0,0 +1,114 @@
+window.Gispro = {}
+window.Gispro.Utils = {
+
+  // translation of fields and layer codes (fedd)
+  translatedSymbols: {
+      field: {},
+      layer: {}
+  },
+
+  translateSymbols: function(symbolType, symbolCodes){
+      var cached = this.translatedSymbols[symbolType];
+      if(cached){
+          var toAsk = [];
+          for(var i=0,len=symbolCodes.length;i<len;i++){
+              if(!cached[symbolCodes[i]]){
+                  toAsk.push(symbolCodes[i]);
+              }
+          }
+          if(toAsk.length>0){
+              //call the servlet
+              var url = "translate";
+              var request = OpenLayers.Request.issue({
+                  method: "GET",
+                  url: url,
+                  async: false,
+                  params:{
+                      type: symbolType,
+                      code: toAsk
+                  }
+              });
+
+              if(request.status==200){
+                  var answered = Ext.util.JSON.decode(request.responseText);
+                  //caching all incoming
+                  for(var prop in answered){
+                      cached[prop] = answered[prop];
+                  }
+                  //caching not founded
+                  for(var i=0,len=symbolCodes.length;i<len;i++){
+                      if(!answered[symbolCodes[i]]){
+                          cached[symbolCodes[i]] = symbolCodes[i]
+                      }
+                  }
+              }
+          }
+      }
+      //var ret = {};
+      //for(i=0;i<symbolCodes.length;i++){
+      //    if(cached && cached[symbolCodes[i]])
+      //        ret[symbolCodes[i]] = cached[symbolCodes[i]];
+      //    else
+      //        ret[symbolCodes[i]] = symbolCodes[i];
+      //}
+
+      //return ret;
+      return cached;
+  },
+
+  // metadata of fields and of layer codes (fedd)
+  metaData: {
+      field: {},
+      layer: {}
+  },
+
+  getMetaData: function(symbolType, symbolCodes){
+//	console.log ('getMetaData : symbolType = ' + symbolType + ', symbolCodes = ' + symbolCodes);
+      var cached = this.metaData[symbolType];
+      if(cached){
+          var toAsk = [];
+          for(var i=0,len=symbolCodes.length;i<len;i++){
+              if(!cached[symbolCodes[i]]){
+                  toAsk.push(symbolCodes[i]);
+              }
+          }
+          if(toAsk.length>0){
+              //call the servlet
+              var url = "metadata";
+              var request = OpenLayers.Request.issue({
+                  method: "GET",
+                  url: url,
+                  async: false,
+                  params:{
+                      type: symbolType,
+                      code: toAsk
+                  }
+              });
+
+              if(request.status==200){
+                  var answered = Ext.util.JSON.decode(request.responseText);
+                  //caching all incoming
+                  for(var prop in answered){
+                      cached[prop] = answered[prop];
+                  }
+                  //caching not founded
+                  for(var i=0,len=symbolCodes.length;i<len;i++){
+                      if(!answered[symbolCodes[i]]){
+                          cached[symbolCodes[i]] = symbolCodes[i]
+                      }
+                  }
+              }
+          }
+      }
+      //var ret = {};
+      //for(i=0;i<symbolCodes.length;i++){
+          //if(cached && cached[symbolCodes[i]])
+              //ret[symbolCodes[i]] = cached[symbolCodes[i]];
+          //else
+              //ret[symbolCodes[i]] = symbolCodes[i];
+      //}
+
+      //return ret;
+      return cached;
+  } 
+}
